0d9e662e3341cec915b512405d043dc780f9aec8
Kevin Exton <kcexton01@gmail.com>
2025-06-25 03:36:06 +0000

    Added another redirect in the (auth) protected routes because the landing page can possibly be served over http from a CDN network.

--- src/routes/(auth)/+layout.ts
+++ src/routes/(auth)/+layout.ts
@@ -1,8 +1,13 @@
+import { redirect } from '@sveltejs/kit';
 import { auth0Guard } from '$lib/services/auth0.service';
 export const ssr = false;
 export const prerender = false;
 
 export async function load({ url }) {
+	if (url.protocol === 'http:' && url.hostname !== 'sveltekit-prerender' && import.meta.env.PROD) {
+		url.protocol = 'https:';
+		return redirect(308, url);
+	}
 	await auth0Guard({
 		redirectTo: url.pathname
 	});

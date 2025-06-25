d3d5b461a32bf3c44a391a768f0e7ba461819352
Kevin Exton <kcexton01@gmail.com>
2025-06-25 03:29:36 +0000

    Added a sveltekit-prerender exception to https redirects so that pages can still be statically prerendered at build time.

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,7 +2,7 @@ import { redirect } from '@sveltejs/kit';
 
 export async function handle({ event, resolve }) {
 	const { url } = event;
-	if (url.protocol === 'http:' && import.meta.env.PROD) {
+	if (url.protocol === 'http:' && url.hostname !== 'sveltekit-prerender' && import.meta.env.PROD) {
 		url.protocol = 'https:';
 		return redirect(308, url);
 	}

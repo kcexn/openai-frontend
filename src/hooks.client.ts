c7da3aa3f14d7af5937b9ff15541ce872a912b8c
Kevin Exton <kcexton01@gmail.com>
2025-06-25 04:16:32 +0000

    After doing the whole runaround, I think using relying on the server-side redirect in the server-side hook is probably still best.

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,14 +1,5 @@
-import { redirect } from '@sveltejs/kit';
 import { initAuth0 } from '$lib/services/auth0.service';
 
 export async function init() {
-	if (
-		window.location.protocol === 'http:' &&
-		window.location.hostname !== 'sveltekit-prerender' &&
-		import.meta.env.PROD
-	) {
-		window.location.protocol = 'https:';
-		redirect(308, window.location.href);
-	}
 	await initAuth0();
 }

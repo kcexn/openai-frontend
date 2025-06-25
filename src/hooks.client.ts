c604b8e5a34252c0e5bdf84f8968cd5f814b8469
Kevin Exton <kcexton01@gmail.com>
2025-06-25 03:59:52 +0000

    Moved the client-side redirect out of the layout and into the clientside init hook.

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,5 +1,9 @@
 import { initAuth0 } from '$lib/services/auth0.service';
 
 export async function init() {
+    if(window.location.protocol === 'http:' && window.location.hostname !== 'sveltekit-prerender' && import.meta.env.PROD) {
+        window.location.protocol = 'https:';
+        window.location.replace(window.location.href);
+    }
 	await initAuth0();
 }
--- src/routes/+layout.ts
+++
@@ -1,8 +0,0 @@
-import { redirect } from '@sveltejs/kit';
-
-export async function load({ url }) {
-	if (url.protocol === 'http:' && url.hostname !== 'sveltekit-prerender' && import.meta.env.PROD) {
-		url.protocol = 'https:';
-		return redirect(308, url);
-	}
-}
